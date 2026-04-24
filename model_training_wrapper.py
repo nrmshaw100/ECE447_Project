fcd0605ded2e6e86263ab334361bc9d6fc5f6290
Nonmiscelaneous <mathewforan@gmail.com>
2026-04-23 22:14:15 -0600

    fixed MLflow logging

--- model_training_wrapper.py
+++ model_training_wrapper.py
@@ -1,5 +1,6 @@
+# Tracking Config
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
 mlflow.sklearn.autolog()
-# Enable MLflow autologging
 
 # wrapper for MLflow logging
 def run_mlflow_experiment(model_trainer, X_train, y_train, X_test, y_test, run_name="Model Experiment"):
@@ -15,21 +16,34 @@ def run_mlflow_experiment(model_trainer, X_train, y_train, X_test, y_test, run_n
     with mlflow.start_run(run_name=run_name) as run:
         # Train model
         model = model_trainer(X_train, y_train)
+        mlflow.sklearn.log_model(model, "model")
         # evaluate performance
-        evaluate_model_numerics(model, X_test, y_test)
+        eval_metrics, _ = evaluate_model_numerics(model, X_test, y_test)
+        mlflow.log_metrics(eval_metrics)
         # Analyze residuals
         predictions = model.predict(X_test)
-        residuals_analysis_numerics(y_test, predictions)
+        res_metrics = residuals_analysis_numerics(y_test, predictions)
+        mlflow.log_metrics(res_metrics)
         # closing
         run_id = run.info.run_id
         print(f"Successfully logged '{run_name}' under Run ID: {run_id}")
-        return model, run_id
+        return model, run_id, eval_metrics, res_metrics
 
 # Usage:
 # model, rid = run_mlflow_experiment(train_decision_tree, X_train, y_train, X_test, y_test, "Decision Tree Regressor")
 
 
+import os
+
 # Hyper parameter tuning
+# Experiment Setup
+experiment_name = "Decision_Tree_Hyperparameter_Tuning"
+artifact_uri = f"file://{os.path.join(os.getcwd(), 'mlruns_artifacts')}"
+
+if not mlflow.get_experiment_by_name(experiment_name):
+    mlflow.create_experiment(name=experiment_name, artifact_location=artifact_uri)
+
+mlflow.set_experiment(experiment_name)
 
     # Depth:
 results = []
@@ -42,7 +56,7 @@ for i in [1, 2, 3, 4, 5]:
         return model
 
     # Run the experiment
-    model, rid = run_mlflow_experiment(
+    model, rid, eval_met, res_met = run_mlflow_experiment(
         train_dt_with_depth, 
         X_train, y_train, 
         X_val, y_val, 
@@ -53,8 +67,6 @@ for i in [1, 2, 3, 4, 5]:
     results.append({
         "max_depth": i,
         "run_id": rid,
-        "rmse": eval_metrics['rmse'],
-        "skew": res_metrics['res_skew']
+        "rmse": eval_met['rmse'],
+        "skew": res_met['res_skew']
     })
-
-
